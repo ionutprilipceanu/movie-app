bebfb0fb37687f71a80fecaa43f3996cfb460375
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2021-02-07 19:55:37 +0200

    up

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
 
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
-//const APIURL = 'https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22';
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
-const main = document.querySelector('main');
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 
-async function getMovies() {
-  const resp = await fetch(APIURL);
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
   const respData = await resp.json();
   
   console.log(respData);
 
-  respData.results.forEach(movie => {
-    const {backdrop_path, title, vote_average} = movie
+  showMovies(respData.results);
+
+}
+
+function showMovies(movies) {
+
+  main.innerHTML = '';
+
+  movies.forEach(movie => {
+    const {poster_path, title, vote_average, overview} = movie
     const movieEl = document.createElement('div');
     movieEl.classList.add('movie');
 
     movieEl.innerHTML = 
     `
       <img 
-      src="${IMGPATH + backdrop_path}" 
+      src="${IMGPATH + poster_path}" 
       alt="${title}"/>
       <div class="movie-info">
         <h3>${title}</h3>
         <span class="${getClassByRate(vote_average)}">${vote_average}</span>
       </div>
-
+      <div class="overview">
+        <h4>Overview<h4>
+        ${overview}
+      </div>
     `;
     main.appendChild(movieEl);
   });
-  return respData;
 }
 
 function getClassByRate(vote){
@@ -41,4 +56,17 @@ function getClassByRate(vote){
   }
 }
 
-getMovies();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if(searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+
+    search.value = '';
+  }
+});
+
+
+
