ce90c1a4de3c556fa751cfd262ef5e12f294a735
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2021-02-07 12:34:02 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+//const APIURL = 'https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22';
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+const main = document.querySelector('main');
+
+async function getMovies() {
+  const resp = await fetch(APIURL);
+  const respData = await resp.json();
+  
+  console.log(respData);
+
+  respData.results.forEach(movie => {
+    const {backdrop_path, title, vote_average} = movie
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = 
+    `
+      <img 
+      src="${IMGPATH + backdrop_path}" 
+      alt="${title}"/>
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+
+    `;
+    main.appendChild(movieEl);
+  });
+  return respData;
+}
+
+function getClassByRate(vote){
+  if(vote >= 8) {
+    return 'green';
+  } else if (vote >= 6) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
+
+getMovies();
